a1ac6989bfe3cb85de8960a6c9a0d6d386b67cf1
Thierry Chekouo <tchekouo@umn.edu>
2024-04-05 23:55:54 -0500

    Update MainBVSSemi.R

--- R/MainBVSSemi.R
+++ R/MainBVSSemi.R
@@ -60,6 +60,7 @@ MainBVSSemi<-function(Method="BVSSemiMRF",Y=Y,X=X,Xcov=NULL,seed=1,atheta=1,bthe
       sigma2=Sigma2(NR,Gamma1F$uSu,aa,ba)
         ### Sample Gamma2 binary model
        Gamma2F=SampleGamma(Gamma=Gamma2,y=U, X=X,Xcov=Xcov,pc=pc,sigma2=1,tau2=tau22,Bigtau2=Bigtau2,theta=theta,Gamma2=Gamma1,nu=nu2)
+      print(theta)
      #  Gamma2=Gamma2F$Gamma
       # beta=Gamma2F$beta
     } else if (Method=="BVSSemiComb") {
